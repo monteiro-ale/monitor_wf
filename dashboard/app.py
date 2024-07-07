dcc340fb8bec0394ab407ec7627e6648babf46b6
monteiro-ale <monteiro.tec32@gmail.com>
2024-07-07 17:28:55 -0300

    Testando conexao com banco de dados local

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,10 +1,36 @@
-# app.py
-
 import streamlit as st
+import psycopg2
 
+@st.cache(allow_output_mutation=True)
+def get_database_connection():
+    conn = psycopg2.connect(
+        host="localhost",
+				port="5436",
+        database="airflow-db",
+        user="airflow",
+        password="airflow"
+    )
+    return conn
+
+def fetch_data():
+    conn = get_database_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM turbines;")
+    data = cursor.fetchall()
+    conn.close()
+    return data
+
+# Início
 def main():
-    st.title('Meu Primeiro Dashboard com Streamlit')
-    st.write('Hello, mundo!')
+    st.title('Dashboard com Streamlit e PostgreSQL')
 
-if __name__ == '__main__':
+  
+    data = fetch_data()
+
+
+    st.write("Dados da Tabela:")
+    st.write(data)
+
+if __name__ == "__main__":
     main()
+
