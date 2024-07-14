e8d243cb3b8dc91f8e8709d4d62bef3c17d7e416
monteiro-ale <monteiro.tec32@gmail.com>
2024-07-14 15:55:25 -0300

    Implementando plotagem de variaçao de temperatura ao longo do tempo.

--- dashboard/app.py
+++ dashboard/app.py
@@ -43,6 +43,30 @@ def fetch_data():
         if 'conn' in locals():
             conn.close()
 
+def fetch_sensor_data():
+    conn = get_database_connection()
+    if conn is None:
+        return pd.DataFrame()
+
+    try:
+        cursor = conn.cursor()
+        cursor.execute('''
+            SELECT turbine_id, temperature, timestamp
+            FROM sensors
+            ORDER BY timestamp;
+        ''')
+        rows = cursor.fetchall()
+        columns = [desc[0] for desc in cursor.description]
+        return pd.DataFrame(rows, columns=columns)
+    except psycopg2.Error as e:
+        st.error(f"Error fetching data from the database: {e}")
+        return pd.DataFrame()
+    finally:
+        if 'cursor' in locals():
+            cursor.close()
+        if 'conn' in locals():
+            conn.close()
+
 def plot_alerts_per_turbine(data, selected_alert_type):
     filtered_data = data[data['alert_type'] == selected_alert_type]
        
@@ -57,6 +81,37 @@ def plot_alerts_per_turbine(data, selected_alert_type):
     else:
         st.write("No data available for the selected alert type")
 
+def plot_temperature_variation(data):
+    fig = px.line(data, x='timestamp', y='temperature', color='turbine_id',
+                  title='Variação da Temperatura ao Longo do Tempo',
+                  labels={'timestamp': 'Período de Tempo', 'temperature': 'Temperatura'})
+
+    fig.add_shape(
+        type="line",
+        x0=data['timestamp'].min(),
+        y0=24,
+        x1=data['timestamp'].max(),
+        y1=24,
+        line=dict(color="Red", width=2, dash="dash"),
+        name="Temperatura Crítica"
+    )
+
+    fig.add_annotation(
+        x=data['timestamp'].min(),
+        y=24,
+        xref="x",
+        yref="y",
+        text="Crítica (24°C)",
+        showarrow=False,
+        font=dict(color="Red"),
+        align="left",
+        xshift=-60,  
+        yshift=10
+    )
+
+    st.plotly_chart(fig)
+
+
 def plot_alert_distribution(data):
     fig = px.pie(data, names='alert_type', values='alert_count', title='Distribuição de Alertas por Tipo')
     st.plotly_chart(fig)
@@ -68,10 +123,15 @@ def plot_stacked_bar(data):
 
 def main():
     st.title("Wind Turbine Monitoring Dashboard")
+    
     data = fetch_data()
-
     if data.empty:
         st.write("No data available")
+        return
+    
+    sensor_data = fetch_sensor_data()
+    if sensor_data.empty:
+        st.write("No sensor data available")
         return
 
     st.sidebar.header("Filter")
@@ -86,6 +146,9 @@ def main():
 
     st.header("Stacked Bar Chart of Alerts")
     plot_stacked_bar(data)
+    
+    st.header("Temperature Variation Over Time")
+    plot_temperature_variation(sensor_data)
 
 if __name__ == "__main__":
     main()
